84d6135654530af4d2d3e15a5e3410da53817003
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-09-17 14:39:51 +0200

    Use htpasswd cookbook to manage virtual user file Fix fail login with md5 -> use crypt instead

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          "Apache License, Version 2.0"
 description      "Installs/Configures vsftpd for Secure SSL SFTP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.4.1"
+
+depends 'htpasswd', '~> 0.2.0'
--- providers/user.rb
+++ providers/user.rb
@@ -1,21 +1,16 @@
-#use_inline_resources
-
 # Support whyrun
 def whyrun_supported?
   true
 end
 
-include Chef::DSL::IncludeRecipe
-
 action :add do
-  include_recipe "vsftpd"
- 
-  ruby_block "update #{new_resource }" do
-    block do
-      update
-    end
-    not_if { current_resource.exists }
-    notifies :restart, "service[vsftpd]"
+  run_context.include_recipe 'htpasswd'
+  
+  htpasswd "vsftpd password for user #{new_resource.user}" do
+    file node['vsftpd']['user_passwd_file']
+    user new_resource.user
+    password new_resource.password
+    type 'crypt'
   end
 
   file ::File.join(node[:vsftpd][:user_config_dir], new_resource.user) do
@@ -30,11 +25,11 @@ action :add do
 end
 
 action :remove do
-  bash "Removing #{new_resource.user} from vsftpd authentication" do
-    code %{
-      sed -i '/#{new_resource.user}.*/ d' #{node[:vsftpd][:user_passwd_file]}
-    }
-    notifies :restart, "service[vsftpd]"
+  run_context.include_recipe 'htpasswd'
+
+  htpasswd "Remove vsftpd user #{new_resource.user}" do
+    file node[:vsftpd][:user_config_dir]
+    user new_resource.user
   end
 
   file ::File.join(node[:vsftpd][:user_config_dir], new_resource.user) do
@@ -43,52 +38,7 @@ action :remove do
   end
 end
 
-def load_current_resource
-  include_recipe "vsftpd"
-
-  @current_resource = Chef::Resource::VsftpdUser.new(@new_resource.name)
-  @current_resource.name(@new_resource.name)
-  @current_resource.user(@new_resource.user)
-  @current_resource.password(@new_resource.password)
-  @current_resource.exists = true if already_there?(@current_resource.user, @current_resource.password)
-end
-
-def already_there?(user = @current_resource.user, password = @current_resource.password)
-  fuser = parse_passwd_file(user)
-  return false unless fuser && fuser['salt']
-  cmd = "openssl passwd -1 -salt #{fuser['salt']} #{password}"
-  pass = Mixlib::ShellOut.new(cmd).run_command.stdout.chomp
-  Chef::Log.debug("generate pass #{pass} for #{user}")
-  return fuser['pass'] == pass
-end
-
-def parse_passwd_file(user)
-  begin
-    line = ::File.readlines(node['vsftpd']['user_passwd_file']).select { |line| line =~ user_regex(user) }.first.chomp
-  rescue
-    return nil
-  end
-  Chef::Log.debug("user #{user} regexed line: #{line}")
-  return nil unless line
-  pass = line.split(':',2).last
-  salt = pass.split('$')[2]
-  ret = { "pass" => pass, "salt" => salt }
-  Chef::Log.debug("Return #{ret.inspect} for #{user}")
-  return ret
-end
-
-def user_regex(user)
-  return /^#{user}:/
-end
-
-def update
-  upf = Chef::Util::FileEdit.new(node['vsftpd']['user_passwd_file'])
-  cmd = "openssl passwd -1 #{current_resource.password}"
-  pass = Mixlib::ShellOut.new(cmd).run_command.stdout.chomp
-
-  newline = [current_resource.user, pass].join(':')  
-
-  upf.search_file_delete_line(user_regex(current_resource.user))
-  upf.insert_line_if_no_match(user_regex(current_resource.user), newline)
-  upf.write_file
+def initialize(*args)
+  super
+  @run_context.include_recipe 'vsftpd'
 end
