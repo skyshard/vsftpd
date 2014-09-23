5565de0e9a978b037b1a94f6205c2e00e0932a57
Guilhem Lettron <guilhem.lettron@optiflows.com>
2014-09-23 15:52:34 +0200

    Fix metadate after README renaming

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Gerhard Lazu'
 maintainer_email 'gerhard@lazu.co.uk'
 license 'Apache License, Version 2.0'
 description 'Installs/Configures vsftpd for Secure SSL SFTP'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.5.3'
 
 depends 'htpasswd', '~> 0.2.0'
